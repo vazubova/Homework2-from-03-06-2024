eb503fbe6d96145ad75d340ac1afebfb977e890a
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-02 21:06:48 +0300

    Выполнено первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        byte cats = 110;
+        short towns = 25678;
+        int dogs = 1546875;
+        long frogs = 576499938;
+        float number = 42.46f;
+        double bigNumber = 32.5734d;
+
+        System.out.println(cats+" кошек");
+        System.out.println(towns+" городов");
+        System.out.println(dogs+" собак");
+        System.out.println(frogs+" лягушек");
+        System.out.println("число "+number);
+        System.out.println("большое число "+bigNumber);
+
     }
 }
